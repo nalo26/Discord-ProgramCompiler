df8d364456a4b562202149970fb238bbee13d2f8
nalo_ <37667081+nalo26@users.noreply.github.com>
2020-04-21 15:57:30 +0200

    Only mp, name should be an exercice name, debug info

--- ProgramCompiler.py
+++ ProgramCompiler.py
@@ -6,8 +6,6 @@ import compiler
 
 client = commands.Bot(command_prefix = "pc!")
 
-SUBMIT_CHANNEL = [701830495629213738] # id of code submition channels
-
 @client.event
 async def on_ready():
     print("Connected as")
@@ -19,26 +17,31 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author.id == client.user.id: return
-    if message.channel.id not in SUBMIT_CHANNEL: return
+    if not isinstance(message.channel, discord.DMChannel): return # only private message
     if len(message.attachments) == 0: return
 
     file = message.attachments[0]
     filename = file.filename
     extension = '.'+filename.split('.')[-1] if len(filename.split('.')) > 1 else ''
     if extension == '': return
-    try: os.mkdir(f"{message.channel.name}/{message.author.id}")
+
+    author = message.author.id
+    exercice = ".".join(filename.split('.')[:-1])
+
+    if exercice in ['.git', '__pycache__', 'users'] or not os.path.isdir(exercice):
+        await client.get_user(author).send(f":x: **Aucun exercice du nom de `{exercice}` n'existe !**")
+        return
+
+    try: os.mkdir(f"{exercice}/{author}")
     except FileExistsError: pass
-    print(f"'{filename}' by {message.author.name}#{message.author.discriminator} in {message.channel.name}")
-    path = f"{message.channel.name}/{message.author.id}"
+    print(f"'{filename}' by {message.author.name}#{message.author.discriminator}")
+    path = f"{exercice}/{author}"
     await file.save(f"{path}/{filename}")
     print(f"file '{filename}' saved to '{path}/{filename}'")
 
-    author = message.author.id
-    
-    await message.delete()
     msg = await client.get_user(author).send(f"Téléchargement du programme {filename}...")
     
-    await compiler.compute(path, filename, extension, msg, author)
+    await compiler.compute(path, filename, extension, exercice, msg, author)
     
 
 client.run("TOKEN")
